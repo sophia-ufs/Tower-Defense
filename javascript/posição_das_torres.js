d95ea2c33f6890af8015a47bf1a42944fabe3cfe
Sophia Damm <sophia.mardones@dcomp.ufs.br>
2024-10-10 11:25:19 -0300

    Create posição_das_torres.js

---
+++ javascript/posição_das_torres.js
@@ -0,0 +1,32 @@
+//Nesse arquivo esta definição das coordenadas em que será possível colocar torres defensivas
+
+const dados_do_mapa = []
+
+/* Código Não funcional
+
+Fonte: https://github.com/chriscourses/tower-defense/blob/main/js/index.js
+
+const placementTilesData2D = []
+
+for (let i = 0; i < placementTilesData.length; i += 20) {
+  placementTilesData2D.push(placementTilesData.slice(i, i + 20))
+}
+
+const placementTiles = []
+
+placementTilesData2D.forEach((row, y) => {
+  row.forEach((symbol, x) => {
+    if (symbol === 14) {
+      // add building placement tile here
+      placementTiles.push(
+        new PlacementTile({
+          position: {
+            x: x * 64,
+            y: y * 64
+          }
+        })
+      )
+    }
+  })
+})
+*/
