8042094abf16ec913715f57ce323c495f9ed49b7
Sophia Damm <sophia.mardones@dcomp.ufs.br>
2024-10-11 17:38:38 -0300

    mudança em "posição dos defensores"

--- js/posição_dos_defensores.js
+++ js/posição_dos_defensores.js
@@ -1,5 +1,5 @@
 
-const dados_do_mapa1 = [ 
+const dados_locais_mapa1 = [ 
   0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
   0, 0, 24, 0, 24, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
   0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
@@ -14,6 +14,21 @@ const dados_do_mapa1 = [
   0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0
 ] 
 
+const dados_pers_mapa1 = [
+  1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 8, 3, 1, 1, 1, 4329, 4329, 4329,
+  468, 1, 0, 1, 0, 1, 1, 1, 19, 1, 1, 7, 1, 1, 1, 1, 5, 4329, 4329, 4329,
+  66, 66, 66, 66, 67, 1, 1, 1, 17, 5, 1, 1, 1, 1, 2, 20, 2, 4329, 4329, 4329,
+  82, 82, 82, 74, 75, 0, 3, 1, 1, 1, 1, 2, 12, 2, 15, 24, 1, 4329, 4329, 4329,
+  1, 1, 1, 165, 166, 1, 1, 424, 1, 3, 2, 31, 1, 1, 1, 1, 1, 4329, 4329, 4329,
+  1, 1, 12, 34, 34, 1, 2, 440, 2, 1, 1, 1, 1, 1, 1, 26, 19, 4329, 4329, 4329,
+  1, 0, 1, 34, 42, 55, 1, 456, 632, 0, 1, 1, 0, 3, 1, 1, 1, 4329, 4329, 4329,
+  3, 1, 1, 49, 50, 41, 50, 62, 66, 66, 66, 66, 66, 67, 0, 0, 0, 4329, 4329, 4329,
+  1, 1, 1, 58, 34, 34, 34, 392, 82, 82, 82, 82, 74, 75, 0, 0, 0, 4329, 4329, 4329,
+  1, 1, 1, 19, 1, 1, 1, 408, 1, 1, 1, 26, 73, 75, 1, 1, 0, 4329, 4329, 4329,
+  12, 1, 0, 1, 12, 0, 1, 1, 1, 1, 19, 1, 165, 166, 1, 7, 0, 4329, 4329, 4329,
+  1, 1, 1, 1, 14, 1, 1, 1, 1, 1, 1, 1, 181, 182, 1, 1, 0, 4329, 4329, 4329
+]
+
 const nova_pos_defesa = (x, y, ocupado) => {
   return {
       x : x*64,
@@ -26,10 +41,11 @@ const achar_coordenadas = (num) => {
   const x = num%20
   return nova_pos_defesa(x, y, false)
 }
-const achar_posiçoes_d = ([x, ...xs], pos = 0) => { 
+const achar_posiçoes_d = ([x, ...xs], param, pos = 0) => { 
   if(pos == 240) return []
-  else if(x == 0) return achar_posiçoes_d(xs, pos+1)
-  else return [achar_coordenadas(pos), ...achar_posiçoes_d(xs, pos+1)]
+  else if(x == param) return [achar_coordenadas(pos), ...achar_posiçoes_d(xs, param, pos+1)] 
+  else return achar_posiçoes_d(xs, param, pos+1)
 }
-const pos_defensor = achar_posiçoes_d(dados_do_mapa1)
-console.log(pos_defensor)
+const pos_defensor1 = achar_posiçoes_d(dados_locais_mapa1, 24)
+const pos_pers1 = achar_posiçoes_d(dados_pers_mapa1, 4329)
+console.log(pos_defensor1)
