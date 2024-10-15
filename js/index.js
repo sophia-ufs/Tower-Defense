b2734ace58941460b0c379aa3914d6e7374d2b88
Ana Beatriz Soares Dantas <beatriz.dantas@dcomp.ufs.br>
2024-10-15 20:04:43 -0300

    organizando funções

--- js/index.js
+++ js/index.js
@@ -89,39 +89,3 @@ async function iniciar(){
 
 }
 iniciar()
-
-//Função para criar retangulos (ainda nao funcional)
-function createRectangles() {
-    const numberOfRectangles = 5; // Defina quantos retângulos você deseja
-    for (let i = 0; i < numberOfRectangles; i++) {
-        createRectangle();
-    }
-}
-
-function createRectangle() {
-    const rectangle = document.createElement("div");
-    rectangle.className = "retangulo"; // Classe CSS do retângulo
-    rectangle.style.position = "absolute"; // Posição absoluta para que apareça sobre o canvas
-    rectangle.style.top = "145px"; // Posição inicial vertical
-    rectangle.style.left = "10px"; // Posição inicial horizontal
-    rectangle.style.width = "110px"; // Largura do retângulo
-    rectangle.style.height = "90px"; // Altura do retângulo
-    rectangle.style.backgroundColor = "rgba(255, 0, 98)"; // Cor do retângulo
-    document.body.appendChild(rectangle);
-
-    moveRectangle(rectangle); // Move o retângulo
-}
-
-// Função para mover o retângulo (ainda não funcional)  
-function moveRectangle(rectangle) {
-    let position = 0; // Posição inicial
-    const interval = setInterval(() => {
-        if (position >= canvas_m.width) {
-            clearInterval(interval); // Para o movimento ao alcançar o fim
-            rectangle.remove(); // Remove o retângulo após o movimento
-        } else {
-            position += 2; // Ajuste a velocidade do movimento
-            rectangle.style.left = position + "px"; // Move o retângulo
-        }
-    }, 30); // Ajuste o tempo para alterar a taxa de movimento
-}
